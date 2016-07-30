75d4df485d28f6fd77798c7a87cc6eb70fd01f42
Eric Churchill <eric.churchill2@gmail.com>
2016-07-29 23:48:08 -0700

    Implement base10 and valueAtBit methods in numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -2,11 +2,31 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
+    var binaryNum = ( num >>> 0 ).toString(2);
+    var binaryNumReverse = '';
 
+    for ( var i = binaryNum.length - 1; i >= 0; i-- ) {
+      binaryNumReverse += binaryNum[i];
+    }
+
+    return Number(binaryNumReverse[bit - 1]);
   },
 
   base10: function(str) {
+    var bits = [];
+    var base10Num = 0;
 
+    for ( var i = str.length - 1; i >= 0; i-- ) {
+      bits[bits.length] = Number(str[i]);
+    }
+
+    bits.forEach(function(value, index) {
+      if ( value === 1 ) {
+        base10Num += Math.pow(2, index);
+      }
+    })
+
+    return base10Num;
   },
 
   convertToBinary: function(num) {
